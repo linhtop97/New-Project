a1b01a331719869c469b7fbc085910eb693177ef
NVMinh <nghiemminh0906@gmail.com>
2018-11-09 14:03:09 +0700

    dfhdhfdhfsuaacvity
    g Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/example/linhnb/appwidget/MainActivity.java
+++ app/src/main/java/com/example/linhnb/appwidget/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.linhnb.appwidget;
 
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.util.Log;
+import android.widget.Button;
 
+
+public class MainActivity extends AppCompatActivity {
+  Button button;
+  public static final String TAG = MainActivity.class.getSimpleName();
+    private  String m;
     private String minh = "hihi";
+    private String mPhuongtien;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         for (int i = 0; i < 10; i++) {
             minh += i;
         }
+
+        Log.d(TAG,"Hihi");
+
+
     }
 }
